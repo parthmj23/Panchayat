87aa0bb5d92b5bd8fedae02b6ec9915ba3562aa6
Parth Balothia <parthbalothia@gmail.com>
2024-03-05 12:00:00 +0530

    changes

--- IMasala-main/android/app/src/main/java/com/example/instagram_clone/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.instagram_clone;
-
-import android.content.Context;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-
-}
